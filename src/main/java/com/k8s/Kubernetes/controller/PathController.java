7dea7ed59ea23d90d0c24b876846f314fb4b5b27
jaksonlima <jakson@smartbr.com>
2021-04-20 12:37:44 -0300

    K8S machine info

---
+++ src/main/java/com/k8s/Kubernetes/controller/PathController.java
@@ -0,0 +1,36 @@
+package com.k8s.Kubernetes.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@RestController
+@AllArgsConstructor
+public class PathController {
+
+    private final Environment environment;
+
+    @GetMapping
+    public ResponseEntity path(HttpServletRequest request) {
+        final Map toMap = new HashMap();
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        final Calendar cal = Calendar.getInstance();
+        final TimeZone timeZone = cal.getTimeZone();
+
+        toMap.put("timezone", timeZone.getDisplayName());
+        toMap.put("version", Optional.ofNullable(environment.getProperty("VERSION")).orElse("3.0.0"));
+        toMap.put("author", Optional.ofNullable(environment.getProperty("AUTHOR")).orElse("Jakson Wilson Bonfim de Lima"));
+        toMap.put("locale", request.getLocale().toLanguageTag());
+        toMap.put("machine", request.getLocalName());
+        toMap.put("date", formatter.format(LocalDateTime.now()));
+
+        return ResponseEntity.ok(toMap);
+    }
+}
---
+++ src/test/java/com/k8s/Kubernetes/KubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.Kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
